d365cb029af8b926cf07d26ab33118f3053eacd1
jaydeepmuliyana71272 <35268321+jaydeepmuliyana71272@users.noreply.github.com>
2018-02-23 12:26:41 -0500

    Add files via upload

---
+++ Day 4 (23-02-2018)/table/table/MenuItem.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItem.swift
+//  table
+//
+//  Created by MacStudent on 2018-02-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class MenuItem
+{
+    let names : [String] = ["Margherita Pizza","BBQ Chicken Pizza","Sausage Pizza","Mushroom Deep Dish"]
+    
+    let price:[Double] = [7.95,11.49,8.45,12.75]
+    
+    let special:[Bool] = [false,true,false,false]
+}
